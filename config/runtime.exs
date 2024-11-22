12e4f9beba48d29a6fe395d7ae8da6abbd1cd88a
remote-remote <188308486+remote-remote@users.noreply.github.com>
2024-11-22 13:05:31 -0600

    mailer

--- config/runtime.exs
+++ config/runtime.exs
@@ -103,10 +103,13 @@ if config_env() == :prod do
   # Also, you may need to configure the Swoosh API client of your choice if you
   # are not using SMTP. Here is an example of the configuration:
   #
-  #     config :order, Order.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
+  config :order, Order.Mailer,
+    adapter: Swoosh.Adapters.SMTP,
+    relay: "smtp.mailersend.net",
+    username: System.get_env("SMTP_USERNAME"),
+    password: System.get_env("SMTP_PASSWORD"),
+    port: 587
+
   #
   # For this example you need include a HTTP client required by Swoosh API client.
   # Swoosh supports Hackney and Finch out of the box:
